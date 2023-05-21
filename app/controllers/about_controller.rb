2fee4ab65f2988fee7591be49f075c5c69dc2d37
mik91 <41644495+mik91@users.noreply.github.com>
2023-05-21 10:56:39 -0400

    CRUD for the About page

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -7,4 +7,44 @@ class AboutController < ApplicationController
   def show
     @about = About.find(params[:id])
   end
+
+  def new
+    @about = About.new
+  end
+
+  def create
+    @about = About.new(about_params)
+
+    if @about.save
+      redirect_to @about
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @about = About.find(params[:id])
+  end
+
+  def update
+    @about = About.find(params[:id])
+
+    if @about.update(about_params)
+      redirect_to @about
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @about = About.find(params[:id])
+    @about.destroy
+
+    redirect_to about_index_path, status: :see_other
+  end
+
+  private
+    def about_params
+      params.require(:about).permit(:title, :body)
+    end
 end
